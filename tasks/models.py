8d1e2b71a06304695af14b009ced0f8587f4554e
Paras Gupta <guptaparas039@gmail.com>
2022-06-02 23:07:23 +0530

    create Task model with required fields

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    email = models.EmailField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.name} | {self.createdAt}"
