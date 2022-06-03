8408e1b6c94614f7af7b112ea607c570ed985ff1
Paras Gupta <guptaparas039@gmail.com>
2022-06-03 09:08:09 +0530

    add route for home view

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', view=views.index, name="home"),
     path('task/all/', view=views.get_all_task, name="get-tasks"),
     path('task/new/', view=views.add_new_task, name="create-task")
 ]
